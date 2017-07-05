17add35239b6d9524d29aa9bbccea6691521b7e7
Avrumi Friedman <info@lakewoodprogramming.com>
2017-07-04 21:23:46 -0400

    Initial commit

---
+++ AuthDemo.Data/UserAuthDb.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthDemo.Data
+{
+    public class UserAuthDb
+    {
+        private string _connectionString;
+
+        public UserAuthDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddUser(User user, string password)
+        {
+            string salt = PasswordHelper.GenerateSalt();
+            string passwordHash = PasswordHelper.HashPassword(password, salt);
+            user.PasswordSalt = salt;
+            user.PasswordHash = passwordHash;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Users (FirstName, LastName, Email, PasswordHash, Salt)" +
+                                      " VALUES (@firstName, @lastName, @email, @hash, @salt)";
+                command.Parameters.AddWithValue("@firstName", user.FirstName);
+                command.Parameters.AddWithValue("@lastName", user.LastName);
+                command.Parameters.AddWithValue("@email", user.Email);
+                command.Parameters.AddWithValue("@hash", user.PasswordHash);
+                command.Parameters.AddWithValue("@salt", user.PasswordSalt);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public User Login(string email, string password)
+        {
+            User user = GetByEmail(email);
+            if (user == null)
+            {
+                return null;
+            }
+            bool isCorrectPassword = PasswordHelper.PasswordMatch(password, user.PasswordSalt, user.PasswordHash);
+            if (!isCorrectPassword)
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        public User GetByEmail(string email)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Users WHERE Email = @email";
+                command.Parameters.AddWithValue("@email", email);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (!reader.Read())
+                {
+                    return null;
+                }
+
+                return GetUserFromReader(reader);
+            }
+        }
+
+        private User GetUserFromReader(SqlDataReader reader)
+        {
+            User user = new User
+            {
+                Id = (int)reader["Id"],
+                FirstName = (string)reader["FirstName"],
+                LastName = (string)reader["LastName"],
+                Email = (string)reader["Email"],
+                PasswordHash = (string)reader["PasswordHash"],
+                PasswordSalt = (string)reader["Salt"]
+            };
+            return user;
+        }
+    }
+}
--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using AuthDemo.Data;
+using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
@@ -13,18 +16,51 @@ namespace WebApplication4.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Signup()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Signup(User user, string password)
         {
-            ViewBag.Message = "Your contact page.";
+            var db = new UserAuthDb(Properties.Settings.Default.ConStr);
+            db.AddUser(user, password);
+            return Redirect("/");
+        }
 
+        public ActionResult Login()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var db = new UserAuthDb(Properties.Settings.Default.ConStr);
+            var user = db.Login(email, password);
+            if (user == null)
+            {
+                return Redirect("/home/login");
+            }
+            FormsAuthentication.SetAuthCookie(email, true);
+            return Redirect("/home/secret");
+        }
+
+        [Authorize]
+        public ActionResult Secret()
+        {
+            string email = User.Identity.Name; //will always match the first argument in SetAuthCookie
+            var db = new UserAuthDb(Properties.Settings.Default.ConStr);
+            User user = db.GetByEmail(email);
+            return View(new SecretPageViewModel {User = user});
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+
     }
 }
